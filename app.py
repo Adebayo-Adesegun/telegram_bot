8277a88191c420d9f75659f6e28629f5ecf85129
adesegun.adebayo <dadebayo.adesegun@gmail.com>
2023-11-09 22:56:12 +0100

    update to test redeploy trigger to render

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def respond():
    if text == "/start":
        # print the welcoming message
        bot_welcome = """
-       Welcome to omnikado bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       Welcome to omnikado bot test, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
        """
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
